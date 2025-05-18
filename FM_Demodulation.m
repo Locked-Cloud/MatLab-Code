fb80691e4b890b0d185cc0f45aa0508eb668d911
Ibrahim Ahmed <142117977+Locked-Cloud@users.noreply.github.com>
2025-05-18 21:34:01 +0300

    Update FM_Demodulation.m

--- FM_Demodulation.m
+++ FM_Demodulation.m
@@ -1,46 +1,40 @@
 % --- Parameters ---
-fs = 10000;             % Sampling frequency (Hz)
-T = 1;                  % Signal duration (seconds)
-t = 0:1/fs:T;           % Time vector
-fm = 5;                 % Message frequency (Hz)
-fc = 200;               % Carrier frequency (Hz)
-Am = 1;                 % Message amplitude
-Ac = 2;                 % Carrier amplitude
-kf = 200;               % Frequency sensitivity
+fs = 10000;                     % Sampling frequency (Hz)
+t = 0:1/fs:0.1;                 % Time vector (0.1 seconds)
+fm = 100; fc = 1000;            % Message and carrier frequencies (Hz)
+Am = 1; Ac = 2;                 % Amplitudes
+kf = 5000;                      % Frequency sensitivity
 
-% --- Message Signal ---
-m = Am * cos(2*pi*fm*t);   
+% --- FM Modulation ---
+m = Am * cos(2*pi*fm*t);                      % Message signal
+c = Ac * cos(2*pi*fc*t);                      % Carrier signal
+s_fm = Ac * cos(2*pi*fc*t + kf * cumsum(m)/fs); % FM modulated signal
 
-% --- FM Signal ---
-int_m = cumsum(m)/fs;                          % Integrate message
-s_fm = Ac * cos(2*pi*fc*t + 2*pi*kf*int_m);    % FM modulation
-
-% --- Frequency Demodulation (Discriminator method) ---
-diff_fm = [0 diff(s_fm)] * fs;                 % Differentiate FM signal
-envelope = abs(hilbert(diff_fm));             % Extract envelope
-
-% --- DC Removal and Low-Pass Filtering ---
-dc_removed = envelope - mean(envelope);       
-[b, a] = butter(6, 0.02);                      % 6th-order LPF
-m_demod = filter(b, a, dc_removed);           
-
-% --- Normalize Demodulated Signal ---
-m_demod = m_demod * (Am / max(abs(m_demod))); 
+% --- FM Demodulation using Hilbert Transform ---
+analytic_signal = hilbert(s_fm);                    % Complex signal
+inst_phase = unwrap(angle(analytic_signal));        % Instantaneous phase
+inst_freq = [0 diff(inst_phase)] * fs / (2*pi);     % Instantaneous frequency
+m_demod = (inst_freq - fc) / kf;                    % Recover original message
 
 % --- Plotting ---
 figure;
 
-subplot(3,1,1);
+subplot(4,1,1);
 plot(t, m, 'r', 'LineWidth', 1.2);
 title('Original Message Signal');
 xlabel('Time (s)'); ylabel('Amplitude'); grid on;
 
-subplot(3,1,2);
+subplot(4,1,2);
+plot(t, c, 'b');
+title('Carrier Signal');
+xlabel('Time (s)'); ylabel('Amplitude'); grid on;
+
+subplot(4,1,3);
 plot(t, s_fm, 'k');
 title('FM Signal');
 xlabel('Time (s)'); ylabel('Amplitude'); grid on;
 
-subplot(3,1,3);
-plot(t, m_demod, 'b', 'LineWidth', 1.2);
-title('Demodulated Message (Discriminator)');
+subplot(4,1,4);
+plot(t, m_demod, 'g', 'LineWidth', 1.2);
+title('Demodulated Message Signal');
 xlabel('Time (s)'); ylabel('Amplitude'); grid on;
